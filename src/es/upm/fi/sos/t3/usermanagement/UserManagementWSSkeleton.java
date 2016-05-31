ebfa52dab6c4b20af74143609e6e340e18f3c0c5
Javier <jarc0212@gmail.com>
2016-05-31 10:36:48 +0200

    INIT

---
+++ src/es/upm/fi/sos/t3/usermanagement/UserManagementWSSkeleton.java
@@ -0,0 +1,43 @@
+package es.upm.fi.sos.t3.usermanagement;
+
+public class UserManagementWSSkeleton{
+
+	public void logout(	)
+	{
+		//TODO : fill this with the necessary business logic
+
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response login	(
+			es.upm.fi.sos.t3.usermanagement.User user
+			)
+	{
+		//TODO : fill this with the necessary business logic
+		throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#login");
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response addUser (
+			es.upm.fi.sos.t3.usermanagement.User user1
+			)
+	{
+		//TODO : fill this with the necessary business logic
+		throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#addUser");
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response changePassword (
+			es.upm.fi.sos.t3.usermanagement.PasswordPair passwordPair
+			)
+	{
+		//TODO : fill this with the necessary business logic
+		throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#changePassword");
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response removeUser (
+			es.upm.fi.sos.t3.usermanagement.Username username
+			)
+	{
+		//TODO : fill this with the necessary business logic
+		throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#removeUser");
+	}
+
+}
