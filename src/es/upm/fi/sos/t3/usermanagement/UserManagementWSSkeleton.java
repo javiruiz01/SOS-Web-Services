9f5ddb50140a3eed7478fe86e82d3d8ec14c58bb
Javier <jarc0212@gmail.com>
2016-05-31 12:31:56 +0200

    Antes de borrarlo todo

--- src/es/upm/fi/sos/t3/usermanagement/UserManagementWSSkeleton.java
+++ src/es/upm/fi/sos/t3/usermanagement/UserManagementWSSkeleton.java
@@ -8,13 +8,20 @@ import java.util.UUID;
 
 public class UserManagementWSSkeleton{
 
-	private static List<User> users = new ArrayList<User>();
+	// private static List<User> users = new ArrayList<User>();
+	// Necesitamos un hashmap, con las mismas claves que usamos para el connected,
+	// en realidad es pensarlo al reves pero bueno,
+	// Porque sino cuando el root añada el usuario,
+	// No tenemos manera de guardar el userID desde la sesión del root a la del usuario
+	private static Map<UUID, User> users = new HashMap<UUID, User>();
+	
 	private boolean instance = false;
 	// No puede ser un randomUUID, sería el username?
 	// Técnicamente el username es lo mismo que el user.getName()
 	private static Map<UUID, User> connected = new HashMap<UUID, User>();
-	private UUID userID;
+	private UUID userID; // Lo creamos cuando se crea el usuario en addUser
 	private static UUID rootID; // para saber si está conectado
+	private boolean iAmRoot = false;
 	// Podríamos hacer que con el metodo init, cada vez que se cree una sesión
 	// nueva, hacer something
 
@@ -37,15 +44,19 @@ public class UserManagementWSSkeleton{
 			}
 		}
 	}
-
-	// Comprueba si el usuario existe, no si está conectado
+	
 	private boolean exists (User user) {
 		boolean result = false;
-		for (User user1: users) {
-			if (user1.getName().equals(user.getName())) {
-				result = true;
-				break;
-			}
+		if (users.containsValue(user)) {
+			result = true;
+		}
+		return result;
+	}
+	
+	private UUID whoami() {
+		UUID result = this.userID;
+		if (userID.equals(rootID)) {
+			iAmRoot = true;
 		}
 		return result;
 	}
@@ -62,14 +73,16 @@ public class UserManagementWSSkeleton{
 	{
 		// Hay que validar el usuario?? --> YES YES
 		Response response = new Response();
-		if ((exists(user) == false) || (connected.get(user) != null)) {
+		User user1 = new User();
+		// get: returns the value to which the specified key is mapped, or null if this map contains no mapping for the key
+		if ((exists(user) == false) || ((user1 = connected.get(userID)) == null)) {
 			response.setResponse(false);
 		} else {
 			// Comprobación de la contraseña
-			
-			userID = UUID.randomUUID();
-			connected.put(userID, user);
-			response.setResponse(true);
+			if (user1.getPwd().equals(user.getPwd())) {
+				connected.put(userID, user); // Lo metemos en el mapa de conectados
+				response.setResponse(true);
+			}
 		}
 		return response;
 	}
@@ -78,11 +91,14 @@ public class UserManagementWSSkeleton{
 			es.upm.fi.sos.t3.usermanagement.User user1
 			)
 	{
+		// Hay que comprobar si somos root
 		Response response = new Response();
 		if (exists(user1) == false) {
 			response.setResponse(false);
 		} else {
-			users.add(user1);
+			
+			userID = UUID.randomUUID();
+			users.put(userID, user1);
 			response.setResponse(true);
 		}
 		return response;
@@ -94,9 +110,10 @@ public class UserManagementWSSkeleton{
 	{
 		// Como sé quien llama a esta funcion?
 		// Y lo mismo para todas las que deberían ser solo llamadas por el superuser
+		Response response = new Response();
+		
 
 		// Comprobar si mi userID está en connected, y luego comprobar que soy el root
-		Response response = new Response();
 		return response;
 	}
 
@@ -104,15 +121,16 @@ public class UserManagementWSSkeleton{
 			es.upm.fi.sos.t3.usermanagement.Username username
 			)
 	{
+		// Comprobar si soy root
 		Response response = new Response ();
-		response.setResponse(false);
-		for (User userToDelete: users) {
-			if (userToDelete.getName().equals(username)) {
-				response.setResponse(true);
-				users.remove(userToDelete);
-				return response;
-			}
-		}
+//		response.setResponse(false);
+//		for (User userToDelete: users) {
+//			if (userToDelete.getName().equals(username)) {
+//				response.setResponse(true);
+//				users.remove(userToDelete);
+//				return response;
+//			}
+//		}
 		return response;
 	}
 }
