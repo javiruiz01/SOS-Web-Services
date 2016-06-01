37a7da1d91cc5bd028dcd4323c1250e2660d2fec
Javier <jarc0212@gmail.com>
2016-06-01 13:34:46 +0200

    Apparently Working
    Primer cliente hecho, haremos más clases para probar diferentes
    situaciones

---
+++ UserManagementService/src/es/upm/fi/sos/t3/usermanagement/UserManagementWSSkeleton.java
@@ -0,0 +1,175 @@
+package es.upm.fi.sos.t3.usermanagement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserManagementWSSkeleton{
+
+	private static Map<String, User> users = new HashMap<String, User>();
+	private static boolean instance = false;
+	private static List<User> connected = new ArrayList<User>();
+	private static User root = new User();
+	private static String usernameRoot; 
+	private User userID;
+
+	public UserManagementWSSkeleton() {
+		// Hay que ver bien cuando se ejecuta el constructor, en principio lo 
+		// Hace axis2, pero no sabemos cuando
+		if ((!instance) && (connected.isEmpty())) {
+			root.setName("admin");
+			root.setPwd("admin");
+			usernameRoot = root.getName();
+			users.put(usernameRoot, root);
+			// connected.add(root);
+			instance = true;
+			// Como el root se añade solo al mapa de usuarios que existen
+			// Se le asignará el userID cuando haga login
+			// this.userID = root;
+		}
+	}
+	
+	public void logout(	)
+	{
+		// Solo borramos de la lista de connected
+		// Ya que el usuario sigue existiendo, aunque no esté conectado
+		for (User userOut: connected) {
+			if (same(userOut, this.userID)) {
+				connected.remove(userOut);
+			}
+		}
+		printList();
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response login	(
+			es.upm.fi.sos.t3.usermanagement.User user
+			)
+	{
+		Response response = new Response();
+		response.setResponse(false);
+		String username = user.getName();
+		// Hay que comprobar que el usuario no está conectado ya y que existe
+		if (!isConnected(user) && users.containsKey(username)) {
+			User user1 = users.get(username);
+			// Comprobamos credenciales
+			if (user1.getPwd().equals(user.getPwd())) {
+				connected.add(user);
+				printList();
+				response.setResponse(true);
+				this.userID = user;
+			}
+		}
+		return response;
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response addUser (
+			es.upm.fi.sos.t3.usermanagement.User user1
+			)
+	{
+		Response response = new Response();
+		// Username es el nombre del usuario
+		String username1 = user1.getName();
+		// Comprobamos si somos root y que el usuario no existía ya
+		if (iAmRoot() && (!users.containsKey(username1))) {
+			// Añadimos al usuario a la lista de creados
+			users.put(username1, user1);	
+			response.setResponse(true);
+			printMap();
+		} else {
+			response.setResponse(false);
+		}
+		return response;
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response changePassword (
+			es.upm.fi.sos.t3.usermanagement.PasswordPair passwordPair
+			)
+	{
+		Response response = new Response();
+		response.setResponse(false);
+		// Comprobar si mi userID está en connected, y luego ver si soy root
+		if (iAmRoot() && isConnected(this.userID)) {
+			// Comprobamos que la oldPwd de passwordPair 
+			// Se corresponde con la contraseña del usuario
+			if (this.userID.getPwd().equals(passwordPair.getOldpwd())) {
+				// Si entramos aquí, es que coinciden y está bien
+				response.setResponse(true);
+				this.userID.setPwd(passwordPair.getNewpwd());
+				// Falta actualizar el mapa y la lista?
+				// Empezamos por el mapa
+				users.put(this.userID.getName(), this.userID);
+				printMap();
+				// Y ahora la lista
+				for (User update: connected) {
+					if (same(update, this.userID)) {
+						connected.remove(update);
+						connected.add(this.userID);
+					}
+				}
+				printList();
+			}
+		}
+		return response;
+	}
+
+	public es.upm.fi.sos.t3.usermanagement.Response removeUser (
+			es.upm.fi.sos.t3.usermanagement.Username username
+			)
+	{
+		Response response = new Response();
+		response.setResponse(false);
+		String username1 = username.getUsername();
+		// Comprobamos que somos root y que nuestro username está dentro del mapa
+		if (iAmRoot() && users.containsKey(username1)) {
+			// Borramos del mapa al usuario
+			users.remove(username1);
+			printMap();
+			response.setResponse(true);
+		}
+		return response;
+	}
+	
+	private boolean isConnected (User user) {
+		boolean result = false;
+		for (User user1: connected) {
+			if (same(user1, user)) {
+				result = true;
+			}
+		}
+		return result;
+	}
+	
+	private boolean iAmRoot() {
+		return same(this.userID, root) ? true: false;
+	}
+	
+	private boolean same (User user1, User user2) {
+		return (user1.getName().equals(user2.getName()) &&
+				user1.getPwd().equals(user2.getPwd())) ? true: false;
+	}
+	
+	private void printMap () {
+		int i = 0;
+		System.out.println("========================= <HashMap> =========================");
+		for (String name: users.keySet()) {
+			String key = name.toString();
+			String value = users.get(key).getName();
+			String pwd = users.get(key).getPwd();
+			System.out.println("Map entry [" + i + "] -> Username: " + key + "| Name: " + value + "| Password: " + pwd);
+			i += 1;
+		}
+		System.out.println("========================= </HashMap> =========================\n");
+	}
+	
+	private void printList() {
+		int i = 0;
+		System.out.println("========================= <List> =========================");
+		for (User value: connected) {
+			String name = value.getName();
+			String pwd = value.getPwd();
+			System.out.println("List entry [" + i + "] -> Name: " + name + "| Password: " + pwd);
+		}
+		System.out.println("========================= </List> =========================\n");
+	}
+}
---
+++ UserManagementServiceClient/src/es/upm/fi/sos/t3/usermanagement/client/UserManagementServiceClient.java
@@ -0,0 +1,57 @@
+package es.upm.fi.sos.t3.usermanagement.client;
+
+import java.rmi.RemoteException;
+
+import org.apache.axis2.AxisFault;
+
+import es.upm.fi.sos.t3.usermanagement.client.UserManagementWSStub.User;
+
+public class UserManagementServiceClient {
+	
+	public static void main (String[] args) throws RemoteException {
+		// Creamos el stub, que va a ser un cliente, y le decimos que mantenga la sesión
+		UserManagementWSStub stub1 = new UserManagementWSStub();
+		stub1._getServiceClient().engageModule("addressing");
+		stub1._getServiceClient().getOptions().setManageSession(true);
+		
+		User root = new User();
+		root.setName("admin");
+		root.setPwd("admin");
+		
+		System.out.println("First login: " + stub1.login(root).getResponse());
+		
+		// Ahora probamos a añadir un usuario
+		User javier = new User();
+		javier.setName("Javier");
+		javier.setPwd("1qazxsw2");
+		System.out.println("Add user: " + javier.getName() + ": " + stub1.addUser(javier).getResponse());
+		
+		
+		UserManagementWSStub stub2 = new UserManagementWSStub();
+		stub2._getServiceClient().engageModule("addressing");
+		stub2._getServiceClient().getOptions().setManageSession(true);
+		
+		
+		// Hacemos login con el usuario ya creado
+		System.out.println("Hacemos login del usuario: " + javier.getName() + " = " + stub2.login(javier).getResponse());
+		
+		// Ahora vamos a probar a añadir un usuario siendo un usuario normal, tiene que dar falso
+		UserManagementWSStub stub3 = new UserManagementWSStub();
+		stub3._getServiceClient().engageModule("addressing");
+		stub3._getServiceClient().getOptions().setManageSession(true);
+		
+		User yerai = new User();
+		yerai.setName("Yerai");
+		yerai.setPwd("fiestaencasa");
+		System.out.println("Intentamos addUser: " + stub2.addUser(yerai).getResponse());
+		
+		// Ahora vemos que pasa si intentamos hacer un login de un usuario que no está en users, debería dar false
+		System.out.println("Intentamos login: " + stub2.login(yerai).getResponse());
+		
+		// Ahora añadimos a Yerai, cogiendo al root
+		System.out.println("Añadimos usuario: " + yerai.getName() + " = " + stub1.addUser(yerai).getResponse());
+		// Y hacemos login de Yerai
+		System.out.println("login: " + yerai.getName() + " = " + stub3.login(yerai).getResponse());
+	}
+
+}
